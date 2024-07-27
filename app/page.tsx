271a08e59083aa35c141f9f745ec9619dea7ad06
dev-xero <elo48858@gmail.com>
2024-07-27 01:31:17 +0100

    feat: Implement mobile nav menu

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Container from "@/components/Container";
 import Header from "@/components/ui/Header";
+import About from "@/components/About";
 
 export default function Home() {
   return (
     <Container>
-        <Header />
+        <>
+            <Header />
+            <About />
+        </>
     </Container>
   );
 }
---
+++ components/About.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+    return (
+        <main className="mt-12">
+            <h3>About!</h3>
+        </main>
+    )
+}
