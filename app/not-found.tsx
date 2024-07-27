c83a6e02337669d5bbf383ef3c10654fbc6a8e7d
dev-xero <elo48858@gmail.com>
2024-07-27 01:03:34 +0100

    feat: Implement not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,14 @@
+import { DeadIcon } from 'hugeicons-react'
+
+export default function NotFoundPage() {
+    return (
+        <main className="grid place-items-center min-w-full min-h-screen">
+            <div>
+                <h3 className="text-xl font-bold flex items-center gap-2"><DeadIcon size={24} /> 404 | Page Not Found</h3>
+                <p className="font-light text-grey-050 mt-4">
+                    Hmm, seems you got lost. Don&apos;t worry, <a href="/" className="text-blue hover:underline underline-offset-4"> here&apos;s home.</a>
+                </p>
+            </div>
+        </main>
+    )
+}
--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -1,3 +1,5 @@
+import { GithubIcon, Moon02Icon } from 'hugeicons-react'
+
 export default function Header() {
     return (
         <header className="flex justify-between items-center p-8 bg-bg-dark">
@@ -7,11 +9,17 @@ export default function Header() {
             </h3>
 
             {/* Navigation Links */}
-            <ul className="flex justify-between items-center gap-4 text-grey-050 transition-colors">
+            <ul className="flex justify-between items-center gap-4 text-grey-050 transition-colors text-base">
                 <li className="hover:text-white transition-all hover:underline underline-offset-4"><a href="https://github.com/dev-xero/home" target="_blank">source code</a></li>
                 <li className="hover:text-white transition-all hover:underline underline-offset-4"><a href="#about">about me</a></li>
                 <li className="hover:text-white transition-all hover:underline underline-offset-4"><a href="#projects">projects</a></li>
                 <li className="hover:text-white transition-all hover:underline underline-offset-4"><a href="/blog">blog</a></li>
+            </ul>
+
+            {/* GitHub + Theme */}
+            <ul className="flex items-center gap-4 text-grey-050">
+                <li className="hover:text-white transition-colors"><a href="https://github.com/dev-xero" target="_blank"><GithubIcon size={24} /></a></li>
+                <li className="cursor-pointer hover:text-white transition-colors"><Moon02Icon size={24} /></li>
             </ul>
         </header>
     )
