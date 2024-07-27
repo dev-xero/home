456c9d60358f719a2fe0376f02536e4c3a04d6e2
dev-xero <elo48858@gmail.com>
2024-07-27 10:47:42 +0100

    patch: Update nav overlay z-index

--- components/ui/NavOverlay.tsx
+++ components/ui/NavOverlay.tsx
@@ -1,5 +1,5 @@
 export default function NavOverlay() {
     return (
-        <div className="sm:hidden block fixed inset-0 bg-bg-dark bg-opacity-50 backdrop-blur-sm" />
+        <div className="sm:hidden block fixed inset-0 bg-bg-dark bg-opacity-50 backdrop-blur-sm z-[999]" />
     )
 }
