a4494eae316ae2be21579984f9337f61fedca217
dev-xero <elo48858@gmail.com>
2024-07-27 03:04:12 +0100

    feat: Update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,25 @@
-import type { Metadata } from "next";
-import { Space_Grotesk } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Space_Grotesk } from 'next/font/google';
+import './globals.css';
 
-const space = Space_Grotesk({ subsets: ["latin"] });
+const space = Space_Grotesk({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "ゼロ Xero",
-  description: "Xero's space on the internet.",
+    title: 'ゼロ Xero',
+    description: "Xero's space on the internet.",
+    icons: {
+        shortcut: 'favicon.svg',
+    },
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={space.className}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={space.className}>{children}</body>
+        </html>
+    );
 }
