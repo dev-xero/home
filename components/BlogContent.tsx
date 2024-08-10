e32ba67f67b400077a58b88f185340681d502e30
dev-xero <elo48858@gmail.com>
2024-08-11 00:47:56 +0100

    feat: Improve site layout

--- components/BlogContent.tsx
+++ components/BlogContent.tsx
@@ -4,6 +4,7 @@ import { IBlog } from '@/lib/getblogs';
 import Container from './Container';
 import Header from './ui/Header';
 import MDXRenderer from './mdx/MDXRenderer';
+import { ArrowLeft02Icon } from 'hugeicons-react';
 
 interface IBlogContentProps {
     blog: IBlog;
@@ -20,6 +21,10 @@ export default function BlogContent({ blog }: IBlogContentProps) {
                 />
 
                 <main className="mt-24">
+                    <a href="./blog" className="flex gap-2 items-center text-grey-050 hover:text-white underline-offset-4 hover:underline">
+                        <ArrowLeft02Icon />
+                        <span>Back</span>
+                    </a>
                     <h1 className="mt-12 font-bold text-3xl mb-4">
                         {blog.metadata.title}
                     </h1>
