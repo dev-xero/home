44082b8633a5bf42fbc070c387ebb97967979c5d
dev-xero <elo48858@gmail.com>
2024-08-10 09:40:41 +0100

    test: Disable automatic meta generation

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -6,45 +6,45 @@ import { getBlog } from '@/lib/getblogs';
 import { notFound } from 'next/navigation';
 
 // Metadata optimizations
-export async function generateMetadata({ params }: any) {
-    const blog = await getBlog(params.slug);
+// export async function generateMetadata({ params }: any) {
+//     const blog = await getBlog(params.slug);
 
-    if (!blog) {
-        return {
-            title: 'Page not found.',
-            description: 'This page does not exist.',
-            type: 'website',
-            icons: {
-                shortcut: '/favicon.svg',
-                icon: '/favicon.svg',
-            },
-            images: [
-                {
-                    url: 'https://home-xero.netlify.app/og-img.png',
-                    width: 600,
-                    height: 600,
-                },
-            ],
-        };
-    }
+//     if (!blog) {
+//         return {
+//             title: 'Page not found.',
+//             description: 'This page does not exist.',
+//             type: 'website',
+//             icons: {
+//                 shortcut: '/favicon.svg',
+//                 icon: '/favicon.svg',
+//             },
+//             images: [
+//                 {
+//                     url: 'https://home-xero.netlify.app/og-img.png',
+//                     width: 600,
+//                     height: 600,
+//                 },
+//             ],
+//         };
+//     }
 
-    return {
-        title: blog.metadata.title,
-        description: blog.metadata.summary,
-        type: 'article',
-        icons: {
-            shortcut: '/favicon.svg',
-            icon: '/favicon.svg',
-        },
-        images: [
-            {
-                url: blog.metadata.image,
-                width: 600,
-                height: 600,
-            },
-        ],
-    };
-}
+//     return {
+//         title: blog.metadata.title,
+//         description: blog.metadata.summary,
+//         type: 'article',
+//         icons: {
+//             shortcut: '/favicon.svg',
+//             icon: '/favicon.svg',
+//         },
+//         images: [
+//             {
+//                 url: blog.metadata.image,
+//                 width: 600,
+//                 height: 600,
+//             },
+//         ],
+//     };
+// }
 
 // Blog page content
 export default async function SingleBlogPage({ params }: { params: any }) {
