ea4d3f8890dc3790df42ad994bca8ac1b2dd1728
dev-xero <elo48858@gmail.com>
2024-08-21 00:38:40 +0100

    blog: Start neovim blog

--- components/mdx/RoundedImage.tsx
+++ components/mdx/RoundedImage.tsx
@@ -4,20 +4,22 @@ import Image from "next/image";
 
 interface IRoundedImageProps {
     src: string,
-    alt: string
+    alt: string,
+    context?: string
 }
 
-export default function RoundedImage({ src, alt }: IRoundedImageProps) {
+export default function RoundedImage({ src, alt, context }: IRoundedImageProps) {
     return (
-        <>
+        <div className="my-4 flex flex-col items-center gap-2">
             <Image
                 src={src}
                 alt={alt}
                 width={1020}
                 height={200}
                 priority={true}
-                className="rounded-md my-4"
+                className="rounded-md"
             />
-        </>
+            {context && <span className="my-2 text-grey-050 font-normal w-full text-center">{context}</span>}
+        </div>
     );
 }
