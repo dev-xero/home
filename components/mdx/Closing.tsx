1f0c5ee70362f38e66161e043b55efd7298e96ae
dev-xero <elo48858@gmail.com>
2024-08-21 13:38:18 +0100

    feat: Blog closing section

---
+++ components/mdx/Closing.tsx
@@ -0,0 +1,14 @@
+export default function Closing() {
+  return (
+    <>
+      <section className="mt-8 p-4 border-t-2 border-light-dark">
+        <p className="text-grey-100">
+          Questions, comments or suggestions? Ping me a mail at{" "}
+          <span className="text-white">ssh.xero@gmail.com</span> or{" "}
+          <span className="text-white">@_xerodev</span>{" "}
+          on Discord, and I'll get back to ya!
+        </p>
+      </section>
+    </>
+  );
+}
--- components/mdx/MDXRenderer.tsx
+++ components/mdx/MDXRenderer.tsx
@@ -1,36 +1,38 @@
-'use client';
+"use client";
 
-import { MDXRemote } from 'next-mdx-remote';
-import HeroImage from './HeroImage';
-import RoundedImage from './RoundedImage';
-import H1 from './elements/H1';
-import Ol from './elements/Ol';
-import P from './elements/P';
-import Spacer from './Spacer';
-import H3 from './elements/H3';
-import A from './elements/A';
-import Pre from './elements/Pre';
-import AnimeRatingCard from '../ui/AnimeRatingCard';
+import { MDXRemote } from "next-mdx-remote";
+import HeroImage from "./HeroImage";
+import RoundedImage from "./RoundedImage";
+import H1 from "./elements/H1";
+import Ol from "./elements/Ol";
+import P from "./elements/P";
+import Spacer from "./Spacer";
+import H3 from "./elements/H3";
+import A from "./elements/A";
+import Pre from "./elements/Pre";
+import AnimeRatingCard from "../ui/AnimeRatingCard";
+import Closing from "./Closing.tsx";
 
 export default function MDXRenderer({ source }: { source: any }) {
-    return (
-        <article className="text-grey-050">
-            <MDXRemote
-                suppressHydrationWarning
-                {...source}
-                components={{
-                    HeroImage,
-                    Spacer,
-                    RoundedImage,
-                    AnimeRatingCard,
-                    h1: H1,
-                    h3: H3,
-                    a: A,
-                    ol: Ol,
-                    p: P,
-                    pre: Pre,
-                }}
-            />
-        </article>
-    );
+  return (
+    <article className="text-grey-050">
+      <MDXRemote
+        suppressHydrationWarning
+        {...source}
+        components={{
+          HeroImage,
+          Spacer,
+          RoundedImage,
+          AnimeRatingCard,
+          Closing,
+          h1: H1,
+          h3: H3,
+          a: A,
+          ol: Ol,
+          p: P,
+          pre: Pre,
+        }}
+      />
+    </article>
+  );
 }
