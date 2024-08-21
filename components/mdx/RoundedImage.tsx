d7ef1d4ea3dc279027b499e49440844d5b51cf90
dev-xero <elo48858@gmail.com>
2024-08-21 01:50:07 +0100

    feat: Update rounded image component

--- components/mdx/RoundedImage.tsx
+++ components/mdx/RoundedImage.tsx
@@ -1,25 +1,31 @@
-'use client';
+"use client";
 
 import Image from "next/image";
 
 interface IRoundedImageProps {
-    src: string,
-    alt: string,
-    context?: string
+  src: string;
+  alt: string;
+  context?: string;
 }
 
-export default function RoundedImage({ src, alt, context }: IRoundedImageProps) {
-    return (
-        <div className="my-4 flex flex-col items-center gap-2">
-            <Image
-                src={src}
-                alt={alt}
-                width={1020}
-                height={200}
-                priority={true}
-                className="rounded-md"
-            />
-            {context && <span className="my-2 text-grey-100 font-normal w-full text-center">{context}</span>}
-        </div>
-    );
+export default function RoundedImage(
+  { src, alt, context }: IRoundedImageProps,
+) {
+  return (
+    <div className="my-4 flex flex-col items-center gap-2">
+      <Image
+        src={src}
+        alt={alt}
+        width={1020}
+        height={200}
+        priority={true}
+        className="rounded-md"
+      />
+      {context && (
+        <span className="my-2 text-grey-100 font-normal w-full text-center">
+          {context}
+        </span>
+      )}
+    </div>
+  );
 }
