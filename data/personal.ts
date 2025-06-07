e9484a057bf510d1a5290a32f04e95298a0b6024
= <ssh.xero@gmail.com>
2025-06-07 18:23:49 +0100

    feat: Update date and links
    Signed-off-by: = <ssh.xero@gmail.com>

--- data/personal.ts
+++ data/personal.ts
@@ -1,9 +1,9 @@
 const personal = {
-    url: 'https://home-xero.vercel.app',
-    school_year: '2nd year',
-    public_email: 'ssh.xero',
-    public_discord: '_xerodev',
-    years_programming: 11,
+  url: 'https://sshxero.vercel.app',
+  school_year: '2nd year',
+  public_email: 'ssh.xero',
+  public_discord: '_xerodev',
+  years_programming: 11,
 };
 
 export default personal;
